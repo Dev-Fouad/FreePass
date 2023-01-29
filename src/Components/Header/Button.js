07d48d3d8ed025a674e528637b82d9346905b6f1
fouad ajayi <fouadajayi6@gmail.com>
2023-01-30 00:43:49 +0100

    almost

--- src/Components/Header/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Button (props){
-    return (
-        <button className="px-5 h-12 w-36 font-inter text-[#FFFFFF] border-2 font-medium text-sm rounded-lg ml-80 border-[#FFFFFF] bg-[#8753AD]  max-md:hidden">{props.children}</button>
-    )
-}
-
-export default Button;
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,26 +1,36 @@
 import React from "react"
 
-import Button from "./Button"
+import { BsChevronDown} from "react-icons/bs";
+// import Button from "./UI/Button"
+import Button from "./UI/Button";
 import logo from "../Assets/logo.png"
 
 function Header() {
     return ( 
         <>
-            <div className=" h-24 border-2 border-[#b91c1c] pl-36 pr-40 flex items-center  bg-purple max-md:pl-5">
+            {/* THE HEADER */}
+
+            <div className="h-24  pl-36 pr-40 flex items-center  bg-purple max-md:pl-5 max-lg:pl-3 ">
              {/* the logo */}
                 <img src={logo} alt="" />
 
              {/* The nav links */}
-             <ul className=" ml-8 text-[#E9D7FE]  text-base font-semibold flex space-x-6 font-Raleway max-md:hidden"> 
+             <ul className=" ml-8 gap-3ml-8 text-[#E9D7FE] text-base font-semibold flex  font-Raleway space-x-6 max-md:hidden max-lg:hidden"> 
                 <li><a href="#">Home</a></li>
-                <li><a href="#">Product</a></li>
-                <li><a href="#">WhyFreePass</a></li>
+                <li className="flex space-x-2"><a href="#">Product</a><span className="pt-1"><BsChevronDown/></span></li>
+                <li><a href="#">WhyFreePass</a></li>s
                 <li><a href="#">Pricing</a></li>
-                <li><a href="#">Contact Us</a></li>     
+                <li><a href="#">Contact Us</a></li>
              </ul>
 
              {/* Button */}
              <Button>Sign Up</Button>
+
+            </div>
+            
+
+            {/* The  */}
+            <div>
 
             </div>
         </>

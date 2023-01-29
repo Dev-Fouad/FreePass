bb156723d87a4fa59678ebab85865110e4e76136
fouad ajayi <fouadajayi6@gmail.com>
2023-01-29 19:19:31 +0100

    done

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,13 +1,27 @@
 import React from "react"
-import logo from "../Assets/logo.png";
-import Zero  from "../Assets/Zero.png"
+// import Zero from "../Assets/Zero.png"
+import logo from "../Assets/logo.png"
 
-function Header () {
-    return <>
-        <h2> my name</h2>
-        <img src={logo} alt="" />
-        <img src={Zero} alt="" />
-    </>
+function Header() {
+    return ( 
+        <>
+            <div className=" h-24 w-full border-2 border-[#b91c1c] pl-36 flex items-center lg:bg-purple sm:bg-[#b91c1c]">
+             {/* the logo */}
+                <img src={logo} alt="" />
+
+             {/* The nav links */}
+             <ul className="flex text-[#E9D7FE] border-[#b91c1c] border-2 justify-between">
+                <li>Home</li>
+                <li>Product</li><span></span>
+                <li>WhyFreePass</li>
+                <li>Pricing</li>
+                <li>Contact Us</li>
+             </ul>
+
+
+            </div>
+        </>
+    )
 }
 
 export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    colors: {
-      purple: '#300053',
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      screens: {
+        sm: '480px', 
+        md: '768px',
+        lg: '976px',
+        xl: '1440px'
+      },
+      colors: {
+        purple: '#300053',
+        white: '#E9D7FE'
+      },
+      fontFamily: {
+        Raleway: ["Raleway", "sans-serif"]
+      },
+      extend: {},
     },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 }

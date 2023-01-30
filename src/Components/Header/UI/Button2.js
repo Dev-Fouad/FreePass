ee266bc8330299c70f493c466d5ff05398c9d26f
fouad ajayi <fouadajayi6@gmail.com>
2023-01-30 11:39:26 +0100

    doe

---
+++ src/Components/Header/UI/Button2.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Button2(props) {
+    return (
+      <button className="h-[74px] w-[271px] font-Raleway font-semibold text-xl mt-[90px] text-[#8F64AD] border-2 rounded-lg bg-[#FFFFFF] ">{props.children}</button>
+    )
+}
+
+export default Button2;
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,6 +3,8 @@ import React from "react"
 import { BsChevronDown } from "react-icons/bs";
 import Button from "./UI/Button";
 import logo from "../Assets/logo.png";
+import Heropics from "../Assets/Heropics.png"
+import Button2 from "./UI/Button2";
 
 function Header() {
     return (
@@ -15,11 +17,11 @@ function Header() {
 
                 {/* The nav links */}
                 <ul className=" ml-8 gap-3ml-8 text-[#E9D7FE] text-base font-semibold flex font-Raleway space-x-6   max-md:hidden max-lg:hidden">
-                    <li><a href="#">Home</a></li>
-                    <li className="flex space-x-2"><a href="#">Product</a><span className="pt-1"><BsChevronDown /></span></li>
-                    <li><a href="#">WhyFreePass</a></li>s
-                    <li><a href="#">Pricing</a></li>
-                    <li><a href="#">Contact Us</a></li>
+                    <li>Home</li>
+                    <li className="flex">Product<span className="pt-1 pl-2 "><BsChevronDown /></span></li>
+                    <li>WhyFreePass</li>s
+                    <li>Pricing</li>
+                    <li>Contact Us</li>
                 </ul>
 
                 {/* Button */}
@@ -29,13 +31,20 @@ function Header() {
 
 
             {/* The  Hero*/}
-            <div className=" flex  h-[998px] pl-36 pt-[115px] bg-purple   max-md:pl-[22.5px] max-md:pr-[27.5px] max-md:pt-10  max-lg:pl-12 max-lg:pt-[80px]">
+            <div className="flex border-2 h-[998px] pl-36 pt-[115px] bg-purple   max-md:pl-[22.5px] max-md:pr-[27.5px] max-md:pt-10  max-lg:pl-12 max-lg:pt-[80px]">
+                
               <div>
                 <h2 className="font-Raleway font-semibold text-[47.78px] text-[#FFFFFF] leading-[56.09px]">Zero-rated digital<br/>content delivery<br/>platform for Africa</h2>
                 <p className="font-Raleway pt-[31.5px] text-xl text-[#E8E7E8]"> Deliver <span className="italic font-extrabold text-xl">Free Pass | Zero-Rated .</span> </p>
                 <p className="text-[19.2px] mt-[31.5px] leading-[29px]  font-Raleway font-medium text-[#E8E7E8] w-[436px]">Zero Rate by FreePass  platform is engineered<br/> to power on-demand and real-time crystal clear video, audio contents for underserved and underprivileged Africans.</p>
+                <Button2>Join our Waitlist</Button2>
               </div>
-            </div>
+
+              <div className="">
+               <img src={Heropics} alt="" className="w-[774px] h-[604px] pb-6"/>
+             </div>
+              
+            </div> 
         </>
     )
 }

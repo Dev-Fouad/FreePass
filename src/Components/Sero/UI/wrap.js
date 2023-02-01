8f7da468d1dea1054abd20f11cea3172f6fdb344
fouad ajayi <fouadajayi6@gmail.com>
2023-02-01 02:11:49 +0100

    vb

--- src/Components/Sero/UI/wrap.js
+++ src/Components/Sero/UI/wrap.js
@@ -3,7 +3,7 @@ import React from "react";
 function Wrap({children}) {
     return (
         <>
-            <div className="flex space-x-[23.53px] mt-[46px] max-md:flex-col max-md:space-x-0 max-md:space-y-11">{children}</div>
+            <div className="flex space-x-[23.53px] mt-[46px] max-md:flex-col max-md:space-x-0 max-md:space-y-11 max-lg:flex-col max-lg:space-x-0  max-lg:space-y-10">{children}</div>
         </>
     )
 }
--- src/Components/Sero/index.js
+++ src/Components/Sero/index.js
@@ -8,9 +8,9 @@ import Boks from "../Assets/Group 4375.png"
 function sero () {
     return (
         <>
-            <div className=" h-[755px] bg-[#FAF7FC]  flex items-center justify-center flex-col max-md:h-[1533px]">
-                <h2 className=" w-[571px] font-Raleway font-semibold text-[56px]  leading-[66px] tracking-[-3px]   mr-[35rem] max-md:mr-0 max-md:w-[325px]">Mind blowing Features</h2>
-                <p className=" w-[571px]  font-medium text-[19.2px] leading-[27.5px]  font-Raleway mt-[25.5px] mr-[35rem] max-md:w-[325px] max-md:mr-0">Powering value, equality, access to education, and ending poverty by creating opportunities and jobs for Africa’s young and creative generation.</p>
+            <div className=" h-[755px] bg-[#FAF7FC]  flex items-center justify-center flex-col max-md:h-[1533px] max-lg:h-[1600px]">
+                <h2 className=" w-[571px] font-Raleway font-semibold text-[56px]  leading-[66px] tracking-[-3px]   mr-[35rem] max-md:mr-0 max-md:w-[325px]  max-lg:mr-0">Mind blowing Features</h2>
+                <p className=" w-[571px]  font-medium text-[19.2px] leading-[27.5px]  font-Raleway mt-[25.5px] mr-[35rem] max-md:w-[325px] max-md:mr-0 max-lg:mr-0 ">Powering value, equality, access to education, and ending poverty by creating opportunities and jobs for Africa’s young and creative generation.</p>
 
                <Wrap>
                     <Board image={Book} text1="ZeroRate Educational Access" text2="Conveniently make educational multimedia (video and audio) content accessible to both underserved and underprivileged to build a stronger and educated African next-generation."/>

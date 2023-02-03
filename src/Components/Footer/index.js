1c4abeaad4e6a0912e5a849a41df461de3dffe67
fouad ajayi <fouadajayi6@gmail.com>
2023-02-03 08:32:57 +0100

    gcg

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -4,7 +4,8 @@ import logo from "../Assets/logo.png"
 function footer () {
     return (
         <>
-            <div className="h-[473px] bg-[#4E0A80] justify-evenly flex  items-center ">
+        <div  className="h-[473px] bg-[#4E0A80] justify-center flex  items-center  flex-col">
+            <div className="  w-[1147px]  justify-between flex">
                 <div>
                     <img src={logo} alt=""></img>
                     <p className="mt-[37.7px] font-Raleway  font-semibold text-base text-[#E9D7FE] w-[320px] h-[38px]">ZeroRate (Data free) access to multimedia content for Africans</p>
@@ -22,7 +23,7 @@ function footer () {
                 
                 <div>
                     <h4 className="text-[#B692F6] font-semibold  font-Raleway text-sm mb-[16px] ">Resources</h4>
-                    <ul className=" space-y-[12px]">
+                    <ul className="  mb-[136px] space-y-[12px]">
                       <li className="text-[#E9D7FE] font-Raleway font-medium text-base">Blog</li>
                       <li className="text-[#E9D7FE] font-Raleway font-medium text-base ">Newsletter</li>
                       <li className="text-[#E9D7FE] font-Raleway font-medium text-base">About Us</li>
@@ -32,13 +33,23 @@ function footer () {
 
                 <div>
                     <h4 className="text-[#FFFF] font-Raleway font-semibold text-sm mb-[16px]  w-[360px]">Join Waitlist</h4>
-                    <input type="text" value="Enter your email" className=" rounded-lg text-[#667085] h-[52px] w-[278px] text-base font-Raleway font-normal p-[14px] "  required/>
+                    <input type="text" placeholder="Enter your email" className=" rounded-lg text-[#667085] h-[52px] w-[278px] text-base font-Raleway font-normal p-[14px] "  required/>
                     <button className=" bg-[#7F56D9] w-[66px] h-[52px] text-base p-[10px] rounded-lg text-[#fff] ml-[16px] mb-20">Join</button>
-                </div>
-
-                
-                
+                </div>                
             </div>
+
+            <div>
+                <hr className=" w-[1147px] text-[#7F56D9]"></hr>
+                <div className="flex justify-between text-[#D6BBFB] items-center mt-[32px]">
+                  <p>© 2022 FreePass Africa . All rights reserved.</p>
+                    <ul className="flex space-x-[16px]">
+                        <li>Terms</li>
+                        <li>Privacy</li>
+                        <li>Cookies</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
